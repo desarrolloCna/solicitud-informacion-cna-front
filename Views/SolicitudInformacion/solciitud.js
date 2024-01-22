0c2e30d26f3e72e2031c8ff994cb5549ae5d455b
desarrolloCna <cnadesarrollo@gmail.com>
2024-01-22 12:31:30 -0600

    Agregar validaciones para campos, cui, telefono, correo, y loading de carga

--- Views/SolicitudInformacion/solciitud.js
+++ Views/SolicitudInformacion/solciitud.js
@@ -12,10 +12,12 @@ const descripcionSolicitudTextarea = document.getElementById('descripcionSolicit
 const tipoNotificacionSelect       = document.getElementById('tipoNotificacion');
 
 
-const URL_saveSolicitud = "https://solicitar-informacion-cna.onrender.com/api/saveSolicitud";
+ const URL_saveSolicitud = "https://solicitar-informacion-cna.onrender.com/api/saveSolicitud"; //Produccion
+//const URL_saveSolicitud = "http://localhost:3000/api/saveSolicitud";                         //Desarrollo
 
 
 
+// ----------------------- solicitudes al backend - INICIO  -------------------------------------------
 const SaveSolicitudPost = async (nombreCompleto, sexo, tipoDocumento, numeroDocumento, telefono, correo, etnia, pertenenciaSocial, descripcionSolicitud, tipoNotificacion) => {
     try{
         const data = {
@@ -38,19 +40,8 @@ const SaveSolicitudPost = async (nombreCompleto, sexo, tipoDocumento, numeroDocu
 }
 
 
-// Obtener el texto de la opción seleccionada de un elemento select
-function obtenerTextoSeleccionado(elementoSelect) {
-    // Obtener el índice de la opción seleccionada
-    let indiceSeleccionado = elementoSelect.selectedIndex;
-    
-    // Obtener el texto de la opción seleccionada
-    let textoSeleccionado = elementoSelect.options[indiceSeleccionado].text;
-    
-    return textoSeleccionado;
-}
-
-
 const GuardarSolicitud = async () => {
+
     if(ValidarInputSolicitudes()){
         return;
     }
@@ -71,12 +62,15 @@ const GuardarSolicitud = async () => {
     const prmTipoNotificacion     = obtenerTextoSeleccionado(tipoNotificacionSelect);
     try{
 
+        MostrarLoadingYOculatarScroll();
+
         const res = await SaveSolicitudPost(prmNombreCompleto, prmSexo, prmTipoDocumento, prmNumeroDocumento, prmTelefono, prmCorreo,
                                       prmEtnia, prmPertenenciaSocial, prmDescripcionSolicitud, prmTipoNotificacion);
 
         if(res.affectedRows === 1){
-            // Mostrar el modal de confirmación de datos guardados
+
             LimpiarInput();
+            // Mostrar el modal de confirmación de datos guardados
             $('#confirmacionGuardadoModal').modal('show');
         }
 
@@ -91,49 +85,127 @@ const GuardarSolicitud = async () => {
             $('#errorModal').modal('show');
          }
 
+    }finally{
+       
+        OculatarLoadingYMostrarScroll();
     }
-
-
-    
 
 }
 
+// ----------------------- solicitudes al backend - FIN  -------------------------------------------
+
+
+// ----------------------- MANIPULACION DEL DOM - INICIO ---------------------------------------------
+
+// Obtener el texto de la opción seleccionada de un elemento select
+function obtenerTextoSeleccionado(elementoSelect) {
+    // Obtener el índice de la opción seleccionada
+    let indiceSeleccionado = elementoSelect.selectedIndex;
+    
+    // Obtener el texto de la opción seleccionada
+    let textoSeleccionado = elementoSelect.options[indiceSeleccionado].text;
+    
+    return textoSeleccionado;
+}
+
+//Eliminar Scroll
+const MostrarLoadingYOculatarScroll = () => {
+
+    // Guarda el estado actual del desplazamiento vertical
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    // Aplica el estilo para ocultar el scroll
+    document.body.style.overflow = 'hidden';
+
+    // Guarda el desplazamiento vertical para restaurarlo más tarde
+    document.body.dataset.scrollY = scrollY;
+
+    //Mostrar loading.
+    document.getElementById('loadingIndicator').style.display = 'block';
+}
+
+
+// Restaura el desplazamiento vertical al cerrar o completar la acción que requiere ocultar el scroll
+const  OculatarLoadingYMostrarScroll = () => {
+    const scrollY = parseInt(document.body.dataset.scrollY || 0, 10);
+    document.body.style.overflow = '';
+    window.scrollTo(0, scrollY);
+
+    document.getElementById('loadingIndicator').style.display = 'none';
+  }
 
 
 //Validar campos vacios
 const ValidarInputSolicitudes = () => {
-    if (!nombreCompletoInput.value || !tipoDocumentoSelect.value || !descripcionSolicitudTextarea.value || !tipoNotificacionSelect.value) {
-        let campoVacio = !nombreCompletoInput.value ? '<strong>Nombre Completo</strong>' :
-                         !tipoDocumentoSelect.value ? '<strong>Tipo de Documento</strong>' :
-                         !descripcionSolicitudTextarea.value ? '<strong>Descripción de la Solicitud</strong>' :
-                         !tipoNotificacionSelect.value ? '<strong>Tipo de Notificación</strong>' : 
-                         (!telefonoInput.value && !correoInput.value) ? '<strong>Celular o Correo</strong>'  : '';
+    let campoVacio = "";
 
-    if(tipoDocumentoSelect.value !== "menorEdad" ){
-        campoVacio = !numeroDocumentoInput.value ? '<strong>Numero Documento</strong>': '';
-                    
-    }
-
-    if(!telefonoInput.value && !correoInput.value){
-        campoVacio = '<strong>Celular o Correo</strong>' 
-    }
-
-        $('#errorModal .modal-body').html(`Por favor, completa el campo obligatorio: ${campoVacio}`);
-        $('#errorModal').modal('show');
+    if (!nombreCompletoInput.value) {
+        campoVacio = "<strong>Nombre Completo</strong>";
+    } else if (tipoDocumentoSelect.value !== "menorEdad" && !numeroDocumentoInput.value) {
+        campoVacio = "<strong>Numero Documento</strong>";
+    }else if (!telefonoInput.value) {
+            campoVacio = '<strong>Celular</strong>';
+    } else if (!correoInput.value) {
+        campoVacio = '<strong>Correo</strong>';
+    } else if (!descripcionSolicitudTextarea.value) {
+        campoVacio = "<strong>Descripción de la Solicitud</strong>";
+    }else if (obtenerTextoSeleccionado(tipoDocumentoSelect) == "DPI" && numeroDocumentoInput.value.length != 13){
+        campoVacio = "<strong>CUI debe ser de 13 digitos</strong>";
+        $("#errorModal .modal-body").html(
+            `${campoVacio}`
+        );
+        $("#errorModal").modal("show");
+        return true;
+    }else if (!validarFormatoCorreo(correoInput.value)){
+        campoVacio = "<strong>Formato de correo incorrecto</strong>";
+        $("#errorModal .modal-body").html(
+            `${campoVacio}`
+        );
+        $("#errorModal").modal("show");
+        return true; // Indica que hay un error
+    }else if (telefonoInput.value.length != 8){
+        campoVacio = "<strong>Numero de telefono debe ser de 8 digitos";
+        $("#errorModal .modal-body").html(
+            `${campoVacio}`
+        );
+        $("#errorModal").modal("show");
+        return true; // Indica que hay un error
+    }else if (tipoDocumento == "DPI" && documentoCUI.length != 13){
+        campoVacio = "<strong>Numero de documento debe ser de 13 digitos";
+        $("#errorModal .modal-body").html(
+            `${campoVacio}`
+        );
+        $("#errorModal").modal("show");
         return true; // Indica que hay un error
     }
-    return false; // Indica que todo está bien
-}
 
+
+    if (campoVacio) {
+        $("#errorModal .modal-body").html(
+            `Por favor, completa el campo obligatorio: ${campoVacio}`
+        );
+        $("#errorModal").modal("show");
+        return true; // Indica que hay un error
+    }
+
+    return false; // Indica que todo está bien
+};
 
 
 //Validar tipo de documento
 const ValidarTipoDocumento = () => {
     const numeroDocumentoDiv = document.getElementById("numeroDocumentoDiv");
+    const formatoCuiSpan = document.getElementById("formatoCui"); 
+
     if (tipoDocumentoSelect.value == "menorEdad") {
         numeroDocumentoDiv.style.display = "none";
+        
     } else {
         numeroDocumentoDiv.style.display = "block"; // Oculta el elemento si no es "menorEdad"
+    }
+
+    if(tipoDocumentoSelect.value != "DPI"){
+        formatoCuiSpan.style.display = "none";
     }
 }
 
@@ -156,6 +228,8 @@ const validarFormatoCorreo = (correo) => {
     return regexCorreo.test(correo);
 };
 
+
+
 // Asignar el evento input al campo de correo
 correoInput.addEventListener('input', () => {
     // Obtener el valor del campo de correo
@@ -171,6 +245,34 @@ correoInput.addEventListener('input', () => {
 });
 
 
+// Validacion Numero de CUI
+numeroDocumentoInput.addEventListener('input', () => {
+    const formatoCuiSpan = document.getElementById("formatoCui"); 
+    const documentoCUI = numeroDocumentoInput.value;
+    const tipoDocumento = obtenerTextoSeleccionado(tipoDocumentoSelect);
+
+    // Validar el formato del cui
+    if (tipoDocumento == "DPI" && documentoCUI.length != 13) {
+        formatoCuiSpan.style.display = "block";
+    } else {
+        formatoCuiSpan.style.display = "none";
+    }
+});
+
+
+// Validacion Numero de telefono
+telefonoInput.addEventListener('input', () => {
+    const formatoTelefonoSpan = document.getElementById("formatoTelefono"); 
+    const telefono = telefonoInput.value;
+
+    // Validar el formato del cui
+    if (telefono.length != 8) {
+        formatoTelefonoSpan.style.display = "block";
+    } else {
+        formatoTelefonoSpan.style.display = "none";
+    }
+});
+
 
 //Funcion para limpiar input
 const LimpiarInput = () => {
@@ -179,9 +281,12 @@ const LimpiarInput = () => {
     telefonoInput.value       = "";
     correoInput.value         = "";
     descripcionSolicitudTextarea.value = "";
-    numeroDocumentoInput.value = ";"
+    numeroDocumentoInput.value = "";
 }
 
 
 
 ValidarTipoDocumento();
+
+
+// ----------------------- MANIPULACION DEL DOM - FIN ---------------------------------------------
